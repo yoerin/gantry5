3fb6de442a35ae2f5fd0c958ccd1e288d6fce6d7
Matias Griese <matias@kunena.org>
2016-03-17 14:52:41 +0200

    SCSS compiler: Fix fatal error if stream missing in import check

--- src/classes/Gantry/Component/Stylesheet/CssCompiler.php
+++ src/classes/Gantry/Component/Stylesheet/CssCompiler.php
@@ -264,7 +264,7 @@ abstract class CssCompiler implements CssCompilerInterface
         // Check if any of the imported files have been changed.
         $imports = isset($content['imports']) ? $content['imports'] : [];
         foreach ($imports as $resource => $timestamp) {
-            $import = $locator->findResource($resource);
+            $import = $locator->isStream($resource) ? $locator->findResource($resource) : realpath($resource);
             if (!$import || filemtime($import) != $timestamp) {
                 return true;
             }
